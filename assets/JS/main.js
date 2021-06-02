52fc6bd7d5327502da63394349caa70b034d04b4
Gabriel Fernandes <gabgui2001@gmail.com>
2021-06-02 19:10:21 -0300

    finishing the back-end the project

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,13 +1,14 @@
 
 var UserWeight;
 var GoalWeight;
-var resul;
+var resul; // It is a value the cal who user need loss
+var ResulExercise;
 
 function call(UserWeight){
 
-   UserWeight = document.getElementById("user-weight").value; 
+   UserWeight = Number(document.getElementById("user-weight").value); 
    
-   GoalWeight = document.getElementById("goal-weight").value;
+   GoalWeight = Number(document.getElementById("goal-weight").value);
 
    resul = UserWeight - GoalWeight;
 
@@ -25,6 +26,15 @@ function call(UserWeight){
    
 }
 
-function retorno(){
-    
+// 7,700 cal = 1kg
+
+function ExerciseRun(){
+   //250 cal to  hour 
+   //alert(resul);
+   ResulExercise = Number(resul*7700)/250;
+   //alert("You need run to "+ResulExercise+" hours");
+   
+   document.querySelector('#ResulTheExercise').innerHTML = ResulExercise;
+   
+
 }
